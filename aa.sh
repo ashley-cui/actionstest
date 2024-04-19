ef1d00b5bac254b46c281c92833fd831902abcd5
Ashley Cui <acui@redhat.com>
2024-04-19 14:08:45 -0400

    asdfsa

--- aa.sh
+++ aa.sh
@@ -6,18 +6,22 @@ for artifact in "podman-remote-release-darwin_amd64.zip darwin_amd" \
                 'podman-remote-release-darwin_arm64.zip darwin_arm' \
                 'podman-remote-release-windows_amd64.zip windows_amd' \
                 'podman-remote-static-linux_amd64.tar.gz linux_amd' \
-                'podman-remote-static-linux_arm64.tar.gz linux_arm' 
+                'asdfasdfsadf lasdf' \
+                'podman-remote-static-linux_arm64.tar.gz linux_arm'
 do
-  set -- $artifact 
+  set -- $artifact
   status=$(curl -s -o /dev/null -w "%{http_code}" "${URI}/${1:?}")
+
   if [[ "$status" == "404" ]] ; then
+    echo "${1:?} will be built"
     needsbuild=true
     echo "${2:?}=true"
   else
-    echo "::warning::${artifact} already exists, skipping"
+    echo "::warning::${1:?} already exists, skipping"
   fi
 done
 
+echo $needsbuild
 
 # VERSION='v5.0.1'
 # URI="https://github.com/containers/podman/releases/download/${VERSION}"
@@ -46,12 +50,19 @@ done
 #   echo "sadfsd"
 # fi
 
-
-# artifact_exists () {
-#     status=$(curl -s -o /dev/null -w "%{http_code}" "${URI}/${$1}")
-#     if [[ "$status" == "404" ]] ; then
-#       return 0
-#     else
-#       echo "::warning::$1 already exists, skipping"
-#     fi
-# }
+        #  URI="https://github.com/containers/podman/releases/download/${{steps.getversion.outputs.version}}"
+        #   for artifact in "podman-remote-release-darwin_amd64.zip darwin_amd" \
+        #         'podman-remote-release-darwin_arm64.zip darwin_arm' \
+        #         'podman-remote-release-windows_amd64.zip windows_amd' \
+        #         'podman-remote-static-linux_amd64.tar.gz linux_amd' \
+        #         'podman-remote-static-linux_arm64.tar.gz linux_arm'
+        #   do
+        #     set -- $artifact # Convert the "tuple" into the param args $1 $2...
+        #     status=$(curl -s -o /dev/null -w "%{http_code}" "${URI}/${1:?}")
+        #     if [[ "$status" == "404" ]] ; then
+        #       needsbuild=true
+        #       echo "${2:?}=true"  >> $GITHUB_OUTPUT
+        #     else
+        #       echo "::warning::${artifact} already exists, skipping"
+        #     fi
+        #   done
